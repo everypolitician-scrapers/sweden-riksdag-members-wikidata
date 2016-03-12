9a2cb1d2255224f4a8905e318fc59561a05ddc09
Tony Bowden <tony@tmtm.com>
2016-03-12 09:20:20 +0000

    Add replacements

--- scraper.rb
+++ scraper.rb
@@ -3,10 +3,15 @@
 
 require 'wikidata/fetcher'
 
-names = EveryPolitician::Wikidata.wikipedia_xpath( 
+originals = EveryPolitician::Wikidata.wikipedia_xpath( 
   url: 'https://sv.wikipedia.org/wiki/Lista_%C3%B6ver_ledam%C3%B6ter_av_Sveriges_riksdag_2014%E2%80%932018',
   xpath: '//table[.//th[.="Plats"]]//tr[td]//td[2]//a[not(@class="new")]/@title',
 ) 
 
-EveryPolitician::Wikidata.scrape_wikidata(names: { sv: names })
+replacements = EveryPolitician::Wikidata.wikipedia_xpath( 
+  url: 'https://sv.wikipedia.org/wiki/Lista_%C3%B6ver_ledam%C3%B6ter_av_Sveriges_riksdag_2014%E2%80%932018',
+  xpath: '//table[.//th[.="Ny ledamot"]]//tr[td]//td[position() = last()]//a[not(@class="new")]/@title',
+) 
+
+EveryPolitician::Wikidata.scrape_wikidata(names: { sv: originals | replacements })
 
